9cbfcad7480dd18e6460eaa2dda805d69caffb2c
Maciej Borkowski <maciekbb98@gmail.com>
2018-12-08 23:40:45 +0100

    changed size of window

--- src/pl/drefosapps/Main.java
+++ src/pl/drefosapps/Main.java
@@ -12,7 +12,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/main_view.fxml"));
         primaryStage.setTitle("Expand Math");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
