b4c06398e06081124c879daf5d8990181e7ed597
Maciej Borkowski <maciekbb98@gmail.com>
2018-12-09 01:26:40 +0100

    created tests for class number

---
+++ tests/pl/drefosapps/NumberTest.java
@@ -0,0 +1,23 @@
+package pl.drefosapps;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class NumberTest {
+    Number number = new Number("A", 12);
+
+    @Test
+    public void shouldReturnLetterATest() {
+        assertEquals("A", number.getLabel());
+    }
+
+    @Test
+    public void shouldReturn12Test() {
+        assertEquals(12, number.getValue());
+    }
+
+    @Test
+    public void shouldReturnAeq12Test() {
+        assertEquals("A = 12", number.toString());
+    }
+}
