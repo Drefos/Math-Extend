e821d42143fe43c61fac160d829fa1c349f36237
Maciej Borkowski <maciekbb98@gmail.com>
2018-12-11 19:33:50 +0100

    moved isFunction method to enum Function

--- src/pl/drefosapps/model/Calculator.java
+++ src/pl/drefosapps/model/Calculator.java
@@ -9,17 +9,17 @@ public class Calculator {
         return "";
     }
 
-    private boolean isFunction(String expression) {
-        int begin = 0, end = 0;
-        while('a' <= expression.charAt(end) && expression.charAt(end) <= 'z') end++;
-        String fun = expression.substring(begin, end).toUpperCase();
-        return Arrays.asList(Function.values()).contains(Function.valueOf(fun));
-    }
-
     private enum Function {
         SIN("sin"), COS("cos"), TG("tg"), POW("pow"),
         SQRT("sqrt"), CBRT("cbrt"), LN("ln"), LOG("log"),
         ROUND("round"), CEIL("ceil"), FLOOR("floor");
         Function(String function){}
+
+        public boolean isFunction(String expression) {
+            int begin = 0, end = 0;
+            while('a' <= expression.charAt(end) && expression.charAt(end) <= 'z') end++;
+            String fun = expression.substring(begin, end).toUpperCase();
+            return Arrays.asList(Function.values()).contains(Function.valueOf(fun));
+        }
     }
 }
