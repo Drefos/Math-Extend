4299d7b44460fa6783ee27d9acee02897a8b7b47
Maciej Borkowski <maciekbb98@gmail.com>
2018-12-11 19:26:49 +0100

    added function checking if expression is a function

--- src/pl/drefosapps/model/Calculator.java
+++ src/pl/drefosapps/model/Calculator.java
@@ -1,9 +1,19 @@
 package pl.drefosapps.model;
 
+import java.util.Arrays;
+
 public class Calculator {
 
     public String calculate(String expression) {
+
         return "";
+    }
+
+    private boolean isFunction(String expression) {
+        int begin = 0, end = 0;
+        while('a' <= expression.charAt(end) && expression.charAt(end) <= 'z') end++;
+        String fun = expression.substring(begin, end).toUpperCase();
+        return Arrays.asList(Function.values()).contains(Function.valueOf(fun));
     }
 
     private enum Function {
