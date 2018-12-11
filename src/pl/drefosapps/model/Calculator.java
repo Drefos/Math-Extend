0ccf0a3358a340ca63630abcd8e9bfdbe65c04d2
Maciej Borkowski <maciekbb98@gmail.com>
2018-12-11 01:58:47 +0100

    created inner enum with functions

--- src/pl/drefosapps/model/Calculator.java
+++ src/pl/drefosapps/model/Calculator.java
@@ -5,4 +5,11 @@ public class Calculator {
     public String calculate(String expression) {
         return "";
     }
+
+    private enum Function {
+        SIN("sin"), COS("cos"), TG("tg"), POW("pow"),
+        SQRT("sqrt"), CBRT("cbrt"), LN("ln"), LOG("log"),
+        ROUND("round"), CEIL("ceil"), FLOOR("floor");
+        Function(String function){}
+    }
 }
