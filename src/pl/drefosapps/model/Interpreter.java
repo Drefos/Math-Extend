a023c585cb60a581f3b7b316b17be665f9c75cd1
Maciej Borkowski <maciekbb98@gmail.com>
2018-12-18 17:42:08 +0100

    added constructor for interpreter

--- src/pl/drefosapps/model/Interpreter.java
+++ src/pl/drefosapps/model/Interpreter.java
@@ -1,6 +1,13 @@
 package pl.drefosapps.model;
 
+import java.util.Map;
+
 public class Interpreter {
+    private Calculator calculator;
+
+    public Interpreter(Map<String, Number> variables) {
+        this.calculator = new Calculator(variables);
+    }
 
     public String interpret(String expression) {
         return "";
