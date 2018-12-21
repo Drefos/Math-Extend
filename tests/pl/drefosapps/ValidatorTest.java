0813a5cd41916239b9c182681031a53e5c37e386
Maciej Borkowski <maciekbb98@gmail.com>
2018-12-21 14:46:52 +0100

    created template for validator tests

---
+++ tests/pl/drefosapps/ValidatorTest.java
@@ -0,0 +1,34 @@
+package pl.drefosapps;
+
+import org.junit.jupiter.api.Test;
+import pl.drefosapps.model.Validator;
+
+import java.util.Collections;
+
+public class ValidatorTest {
+    Validator validator;
+
+    public ValidatorTest() {
+        validator = new Validator(Collections.emptyMap());
+    }
+
+    @Test
+    void checkAssignmentTest() {
+    }
+
+    @Test
+    void checkBracketsTest() {
+    }
+
+    @Test
+    void checkVariablesTest() {
+    }
+
+    @Test
+    void checkOperatorsTest() {
+    }
+
+    @Test
+    void checkFunctionsTest() {
+    }
+}
